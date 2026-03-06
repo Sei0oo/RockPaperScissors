f6363fae265c93e7a754c48b6c8582b0f7557eaa
Osei <oseiboache@gmail.com>
2026-03-06 09:27:46 +0000

    created function for obtaining computer choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello world");
+console.log(Math.floor(Math.random() * 3)+ 1);
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3) + 1
+
+    if (num === 1) {
+    console.log("rock");
+} else if (num === 2) {
+    console.log("paper");
+} else {
+    console.log("scissors");
+}
+
+}  
+// to confirm my logic works
+console.log(getComputerChoice());
+
